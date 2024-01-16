9da4dc22313b48e5a1a9d6bfdd8b724a59b2b8dc
hosseint <hosseinjealous@gmail.com>
2024-01-16 10:38:30 +0330

    add basket microservices section

---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,32 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+            
+        }
+        public ShoppingCart(string username)
+        {
+            UserName = username;
+        }         
+
+        public decimal TotalPrice
+        {
+            get { 
+                 decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+
+                }
+              return totalprice;
+            }
+             
+        }
+
+    }
+}
